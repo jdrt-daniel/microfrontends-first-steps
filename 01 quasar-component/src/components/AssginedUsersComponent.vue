12d347ac4069ac7779b3034d8d985e30324711c6
Daniel Rafael <jrafael@hansa.com.bo>
2024-03-05 16:22:04 -0400

    Nuevos cambios

--- 01 quasar-component/src/components/AssginedUsersComponent.vue
+++ 01 quasar-component/src/components/AssginedUsersComponent.vue
@@ -1,28 +1,29 @@
 <template>
-  <q-card class="my-card" flat bordered>
+  <q-card class="my-card">
     <q-card-section horizontal>
       <q-card-section>
         <q-avatar
-          size="70px"
-          font-size="52px"
+          size="60px"
+          font-size="40px"
           color="teal"
           text-color="white"
           icon="person"
         />
       </q-card-section>
       <q-card-section class="q-pt-xs">
-        <div class="text-overline row justify-between w-full">
-          <div>Usuario asignado</div>
+        <div class="row justify-between">
+          <div class="text-grey-6">Usuario asignado</div>
           <div>
             <q-badge
               color="primary"
               text-color="white"
+              outline
               label="Principal"
               class="q-pa-xs"
             />
           </div>
         </div>
-        <div class="text-h5 q-mt-sm q-mb-xs">Nombre del usuario</div>
+        <div class="text-h7 q-mt-sm q-mb-xs">Nombre del usuario</div>
         <div class="text-caption text-grey">
           Division: Sistemas | AMercado: Sistemas 99
         </div>
@@ -33,9 +34,9 @@
 
     <q-card-actions>
       <q-btn flat> Asignarme</q-btn>
-      <q-btn flat color="primary"> Revertir </q-btn>
+      <q-btn flat> Cambiar </q-btn>
       <q-space />
-      <q-btn flat color="primary" label="algun evento" />
+      <!-- <q-btn flat> Revertir </q-btn> -->
     </q-card-actions>
   </q-card>
 </template>
@@ -44,4 +45,7 @@
 </script>
 
 <style scoped>
+.my-card {
+  max-width: 400px;
+}
 </style>
