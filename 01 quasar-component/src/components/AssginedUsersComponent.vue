fb8bb59548052bd051865638e8d07aba51d9ec3a
Daniel Rafael <jrafael@hansa.com.bo>
2024-03-05 10:35:09 -0400

    First Commit

---
+++ 01 quasar-component/src/components/AssginedUsersComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-card class="my-card" flat bordered>
+    <q-card-section horizontal>
+      <q-card-section>
+        <q-avatar
+          size="70px"
+          font-size="52px"
+          color="teal"
+          text-color="white"
+          icon="person"
+        />
+      </q-card-section>
+      <q-card-section class="q-pt-xs">
+        <div class="text-overline row justify-between w-full">
+          <div>Usuario asignado</div>
+          <div>
+            <q-badge
+              color="primary"
+              text-color="white"
+              label="Principal"
+              class="q-pa-xs"
+            />
+          </div>
+        </div>
+        <div class="text-h5 q-mt-sm q-mb-xs">Nombre del usuario</div>
+        <div class="text-caption text-grey">
+          Division: Sistemas | AMercado: Sistemas 99
+        </div>
+      </q-card-section>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions>
+      <q-btn flat> Asignarme</q-btn>
+      <q-btn flat color="primary"> Revertir </q-btn>
+      <q-space />
+      <q-btn flat color="primary" label="algun evento" />
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ 01 quasar-component/src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="q-pa-md"><AssginedUsersComponent /></div>
+</template>
+
+<script setup lang="ts">
+import AssginedUsersComponent from 'src/components/AssginedUsersComponent.vue';
+</script>
+
+<style scoped>
+</style>
